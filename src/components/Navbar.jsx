b3e07d86e24fe772629380b3c4abbf39854fafce
Biswarghya Biswas <iambiswarghya@gmail.com>
2022-11-07 23:53:39 +0530

    theme toggle button added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../static/img/logo1.png"
+import { BsFillSunFill, BsFillMoonFill} from "react-icons/bs";
+import { useColorMode } from "@chakra-ui/react";
+
+
 function Navbar() {
+
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <div className="navbar flex z-50 w-full bg-white text fixed top-0 left-0">
       <div className="icon w-30  py-3 flex justify-center">
@@ -27,7 +34,11 @@ function Navbar() {
           <NavLink to="contact">
             <li>Contact Us</li>
           </NavLink>
+          
         </ul>
+        <div className="cursor-pointer px-20" onClick={() => toggleColorMode()}>
+          {colorMode === "dark" ? <BsFillSunFill/> : <BsFillMoonFill/>}
+        </div>
       </div>
     </div>
   );
