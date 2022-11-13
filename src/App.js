2c0b4fe9ea1ec6e3989bfe91b5078bcfa86b8e8a
Kallyan Singha <singhakallyan@gmail.com>
2022-11-13 08:13:22 +0530

    added landing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import { Routes, Route, Link } from "react-router-dom";
-import logo from "./logo.svg";
+import { Routes, Route} from "react-router-dom";
+
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 import Landing from "./pages/Main/Landing";
 import Events from "./pages/Events/Events";
@@ -18,8 +16,8 @@ import Layout from "./pages/layout";
 function App() {
   return (
     <Routes>
+      <Route index path="" element={<Landing />} />
       <Route path="/" element={<Layout />}>
-        <Route index path="" element={<Home />} />
         <Route index path="home" element={<Home />} />
         <Route path="events" element={<Events />} />
         <Route path="clubs" element={<Clubs />} />
--- src/pages/Main/Landing.js
+++ src/pages/Main/Landing.js
@@ -1,105 +1,27 @@
-import React from 'react'
-
-import Typewriter from 'typewriter-effect';
+import React from "react";
+import Lottie from 'react-lottie';
+import { useNavigate } from "react-router-dom";
+import animationData from '../../static/landing_animation.json';
 
 function Main() {
-  return (
-    <>
-  <div id='particles-js'>
-  <nav className="navbar-nav ml-auto"></nav>
-  <div className="grid">
-    <div className="main">
-      {/* <img src={img1} /> */}
-      <div className="ic">
-        <a
-          target="_blank"
-          href="https://www.facebook.com/nsec.phoenix/"
-          className="facebook"
-        >
-          <i className="fa fa-facebook-f" />
-        </a>
-        <a target="_blank" href="" className="youtube">
-          {" "}
-          <i className="fa fa-youtube" />
-        </a>
-        <a
-          target="_blank"
-          href="https://www.instagram.com/phoenix_nsec/"
-          className="instagram"
-        >
-          {" "}
-          <i className="fa fa-instagram" />
-        </a>
+  const navigate = useNavigate()
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+  return <div className="flex items-center bg-[#EEEEEF] h-[100vh]">
+   <Lottie 
+	    options={defaultOptions}
+        className="h-full w-full"
+      />
+      <div className=" absolute bottom-0 w-full h-20 flex justify-center items-start">
+        <button className="animate-bounce text-xl font-[500] duration-500 shadow-md bg-yellow-500  px-5 py-2 rounded-md" onClick={()=>navigate('/home')}> Let's Start</button>
       </div>
-    </div>
-    <div className="text">
-      <h1 className="phoenix">
-        <div className="typewriter">
-        <Typewriter
-          options={{
-          strings: ["Phoenix", "Robonix", "Eloquense", "Nirmaan", "Cybernix"],
-          autoStart: true,
-          loop: true,
-          //skipAddStyles: true,
-          }}
-        />
-        </div>
-      </h1>
-
-      <h3 className="tag">Come,Let's Rise</h3>
-      <ul>
-        <li>
-          <a className="discover" href="/home">
-            Explore Us
-          </a>
-        </li>
-      </ul>
-      <br />
-      <div></div>
-    </div>
-  </div>
-  <div className="icons">
-    <a
-      target="_blank"
-      href="https://www.facebook.com/nsec.phoenix/"
-      className="facebook"
-    >
-      <i className="fa fa-facebook-f" />
-      Facebook
-    </a>
-    <a
-      target="_blank"
-      href="https://www.youtube.com/channel/UCBy1iIhw34E7YlHQ8tc4rDA"
-      className="youtube"
-    >
-      {" "}
-      <i className="fa fa-youtube" />
-      Youtube
-    </a>
-    <a
-      target="_blank"
-      href="https://www.instagram.com/phoenix_nsec/"
-      className="instagram"
-    >
-      {" "}
-      <i className="fa fa-instagram" />
-      Instagram
-    </a>
-    <a
-      target="_blank"
-      href="https://t.me/phoenix_nsec2020/"
-      className="telegram"
-    >
-      {" "}
-      <i className="fa fa-telegram" />
-      Telegram
-    </a>
-  </div>
-
-  </div>
-</>
-
-  )
+  </div>;
 }
 
-export default Main
+export default Main;
