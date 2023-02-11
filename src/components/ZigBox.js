fe557d8e1e96a65c6ff58b50100702b02cf38abf
Rajit-D <rajitdutta2002@gmail.com>
2023-02-11 22:30:37 +0530

    Events page added

--- src/components/ZigBox.js
+++ src/components/ZigBox.js
@@ -14,19 +14,25 @@ function ZigBox({ title, description, data }) {
         )}
       </div>
       <div className="zigBody w-full md:w-70 ">
-        {data.map(({ title, description, image ,extraButton }, index) => {
-          return (
+        {data.map(({ title, description, image, extraButton, date, wing, isUpcoming, isClub }, index) => {
+          {if(!isUpcoming || isClub){return (
             <div
-              className={`flex ${
-                index % 2 ? "flex-col-reverse md:flex-row" : "flex-col-reverse md:flex-row-reverse"
-              } justify-evenly my-5 items-center`}
+              className={`flex ${index % 2 ? "flex-col-reverse md:flex-row" : "flex-col-reverse md:flex-row-reverse"
+                } justify-evenly my-5 items-center`}
             >
-              <div className="text w-80 md:w-50">
-                <h2 className="text-[1.2rem] md:text-[1.5rem] font-[700]">{title}</h2>
-                <p className="text-[.8rem] md:text-[1rem] font-[500] mt-3 text-slate-500">{description}</p>
-                {extraButton && <button className="border-2 rounded-full px-3 my-3  hover:bg-[#1d50c3] hover:border-white hover:text-white" onClick={()=>navigate(extraButton.link)}>{extraButton.name}</button>}
+              <div className="text w-80 md:w-50 content-center mb-5">
+                <div className="flex flex-row justify-start items-center">
+                  <h2 className="text-[1.2rem] md:text-[1.5rem] font-[700]">{title}</h2>
+                  {wing && <p className="text-[10px] md:text-[14px] gap-6 px-[8px]  mx-3 text-white bg-[#1d50c3] rounded-full">{wing}</p>}
+                </div>
+                {date && <div className="flex flex-row items-center justify-start mt-3 text-gray-700">
+                  <i class="fa-solid fa-calendar-days"></i>
+                  <p className="ml-2 text-[0.8rem] font-medium">{date}</p>
+                </div>}
+                <p className="text-[.8rem] md:text-[1rem] font-[500] mt-4 text-slate-500">{description}</p>
+                {extraButton && <button className="border-2 rounded-full px-3 my-3  hover:bg-[#1d50c3] hover:border-white hover:text-white" onClick={() => navigate(extraButton.link)}>{extraButton.name}</button>}
               </div>
-              <div className="image mb-4 md:mb-0">
+              <div className="image mb-5 md:mb-0">
                 <img
                   src={image}
                   alt=""
@@ -36,7 +42,7 @@ function ZigBox({ title, description, data }) {
                 />
               </div>
             </div>
-          );
+          );}}
         })}
       </div>
     </div>
--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,10 +1,106 @@
-import React from 'react'
-import Comingsoon from '../../components/Comingsoon'
+import React, { useState, useEffect } from 'react'
+import ZigBox from '../../components/ZigBox'
+import { db } from '../../firebase-config'
+import Carousel from 'react-elastic-carousel'
+import {
+  getDocs,
+  collection
+} from 'firebase/firestore'
+import moment from 'moment/moment'
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 900, itemsToShow: 4 },
+];
 
 function Events() {
+  const [eventList, setEventList] = useState([])
+  const eventsRef = collection(db, "events");
+
+  const getEventList = async () => {
+    try {
+      const data = await getDocs(eventsRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setEventList(filteredData);
+      console.log(eventList)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  var day = moment('10-02-2023');
+  console.log(day)
+
+  // console.log(day);
+
+  useEffect(() => {
+    getEventList();
+  }, []);
+
   return (
     <>
-    <Comingsoon/>
+      <style>
+        {`
+            .rec-dot_active {
+              background-color: var(--blue) !important;
+              box-shadow: 0 0 1px 3px var(--blue) !important;
+          }
+          .rec-dot:focus,
+          .rec-dot:hover {
+              box-shadow: 0 0 1px 3px var(--blue) !important;
+          }
+          .rec-dot_active:focus,
+          .rec-dot_active:hover {
+              background-color: var(--blue) !important;
+              box-shadow: 0 0 1px 3px var(--blue) !important;
+          }
+          
+          .rec-arrow {
+              background-color: var(--blue) !important;
+              transition: all 0.3s ease;
+              font-size: 1.1rem !important;
+              color: white !important;
+              width: 1.7rem !important;
+              height: 1.7rem !important;
+              min-width: 1.7rem !important;
+              line-height: 1.7rem !important;
+          }
+          
+          .rec-arrow:hover:not(:disabled) {
+              transform: scale(1.2);
+          }
+        `}
+      </style>
+      <div className="intro-secondary flex gap-12 justify-start z-1 flex-col md:flex-row px-5 ">
+        <div className="w-full md:w-40 text-white">
+          <p className="text-[2rem] md:text-[3rem] font-[800]">Our Events</p>
+        </div>
+      </div>
+      <ZigBox data={eventList} />
+      <div className="flex flex-col justify-center items-center">
+        <h2 className="text-[1.5rem] md:text-[2rem] font-[800]">Upcoming events</h2>
+        <div className='mt-5 mb-5 flex justify-center space-x-9 md:w-[600px] w-[300px]' >
+          <Carousel breakPoints={breakPoints} enableAutoPlay={true} infinite >
+            {eventList.map((event) => {
+              if (event.isUpcoming)
+                return (<div className="bg-gradient-to-r from-cyan-500 to-blue-500 md:w-[400px] w-[200px] h-[196px] rounded-xl flex flex-col justify-center content-start p-3" >
+                  <div>
+                    <h1 className='text-[1.2rem] md:text-[1.5rem] font-[700] text-white'>{event.title}</h1>
+                    <p className='text-white'>{event.description.slice(0, 25)}</p>
+                  </div>
+                  <div className='mt-9'>
+                    <p className='font-[800] text-white'>{event.date}</p>
+                    <p className='font-[500] text-sm font-light'><span className='text-white text-transparent bg-clip-text'>{moment(event.date, "DDMMYYYY").fromNow()}</span></p>
+                  </div>
+                </div>)
+            })}
+          </Carousel>
+        </div>
+      </div >
     </>
   )
 }
