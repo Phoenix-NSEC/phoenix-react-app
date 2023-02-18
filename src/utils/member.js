60fa45f21db98ae14f1c25bc6cb0ae608bd42b1a
Shreyam Maity <sm8967724231@gmail.com>
2023-02-18 18:17:07 +0000

    feat:regustration fxn

--- src/utils/member.js
+++ src/utils/member.js
@@ -1,8 +1,82 @@
 import { storage, db } from "../firebase-config";
-import { doc, addDoc, collection, runTransaction } from "firebase/firestore";
-import { ref } from "firebase/storage";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} from "firebase/firestore";
+import {
+  Timestamp,
+  doc,
+  setDoc,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/storage";
+import { ulid } from "ulid";
+
+//upload image
+const ImageUploader = async (loc, file) => {
+  const fileName = ulid();
+  const storageRef = ref(
+    storage,
+    loc + "/" + fileName + "." + file.name.split(".").pop()
+  );
+
+  // upload file to firestore
+  return await uploadBytes(storageRef, file, { contentType: file.type }).then(
+    (snapshot) => {
+      return getDownloadURL(storageRef).then((url) => {
+        console.log(url);
+        return url;
+      });
+    }
+  );
+};
 
 // Adds members
 export const addMember = async (memberValue, profilePic, transactionPic) => {
-  
+  try {
+    const eId = ulid();
+    const rgistrationRef = doc(db, "registrations", eId);
+    var q = query(
+      collection(db, "registrations"),
+      where("email", "==", memberValue.email)
+    );
+    await getDocs(q)
+      .then(async (snapshot) => {
+        if (snapshot.empty) {
+          var profileUrl = await ImageUploader("images", profilePic);
+          var transactionUrl = await ImageUploader(
+            "transaction-images",
+            transactionPic
+          );
+          await setDoc(
+            rgistrationRef,
+            {
+              createdAt: Timestamp.fromDate(new Date()),
+              ...memberValue,
+              id: eId,
+              profilePic: profileUrl,
+              transactionPic: transactionUrl,
+              isVerified: false,
+              cardGenerated: false,
+            },
+            { capital: true },
+            { merge: true }
+          );
+          return true;
+        } else {
+          console.log("Document with the same email exists");
+          return false;
+        }
+      })
+      .catch((err) => {
+        console.log("Error getting documents", err);
+        return false;
+      });
+      return true;
+  } catch (error) {
+    return false;
+  }
 };
