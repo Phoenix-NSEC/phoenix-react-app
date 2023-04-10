90e6e6c9cec26526ee7fca570f70e8bd5a8be79d
Kallyan01 <singhakallyan@gmail.com>
2023-04-11 02:01:23 +0530

    removed landing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import "./App.css";
 
-import Landing from "./pages/Main/Landing";
 import Events from "./pages/Events/Events";
 import Clubs from "./pages/Clubs/Clubs";
 import Home from "./pages/Home/Home";
@@ -11,14 +10,13 @@ import Coreteam from "./pages/Coreteam/Coreteam";
 import Gallary from "./pages/Imagegallary/Gallary";
 import Blogs from "./pages/Blogs/Blogs";
 import Webteam from "./pages/Webteam/Webteam";
-import Layout from "./pages/layout";
+import Layout from "./pages/Layout";
 import Member from "./pages/member/Member";
 import MemberRegistration from "./pages/Registration/Registration";
 
 function App() {
   return (
     <Routes>
-      <Route index path="" element={<Landing />} />
       <Route path="/" element={<Layout />}>
         <Route index path="home" element={<Home />} />
         <Route path="events" element={<Events />} />
